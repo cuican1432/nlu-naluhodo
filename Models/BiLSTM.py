e429d13f4b400651aa046cbf5cea7ef2e2c195c1
Elaine <cc5048@nyu.edu>
2019-04-26 14:54:37 -0400

    edited models packages

--- Models/BiLSTM.py
+++ Models/BiLSTM.py
@@ -8,49 +8,63 @@ from keras import initializers, regularizers, constraints, optimizers, layers, c
 from keras import backend as K
 from keras.engine import InputSpec, Layer
 
-arguments = {
-    'max_len': 200,
-    'batch_size': 128,
-    'epochs': 100,
-    'learning_rate': 1e-3,
-    'learning_rate_decay': 0,
-    'units': 128,
-    'drop_out_rate': 0.2,
-    'checkpoint_path': 'best_model.hdf5',
-    'early_stop_patience': 10,
-}
 
-def build_bilstm_model(X_train, y_train, X_valid, y_valid, arguments):
+class BiLstm:
 
-    print('Building model...')
+    def __init__(self, X_train, y_train, X_valid, y_valid, embedding_layer):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_valid = X_valid
+        self.y_valid = y_valid 
+        self.embedding_layer = embedding_layer
+        self.arguments  = {
+                    'max_len': 200,
+                    'batch_size': 128,
+                    'epochs': 100,
+                    'learning_rate': 1e-3,
+                    'learning_rate_decay': 0,
+                    'units': 128,
+                    'drop_out_rate': 0.2,
+                    'checkpoint_path': 'best_model.hdf5',
+                    'early_stop_patience': 10,
+                }
 
-    file_path = arguments['checkpoint_path']
-    check_point = ModelCheckpoint(file_path, monitor = "val_loss", verbose = 1,
-                                  save_best_only = True, mode = "min")
-    ra_val = RocAucEvaluation(validation_data=(X_valid, y_valid), interval = 1)
-    early_stop = EarlyStopping(monitor = "val_loss", mode = "min", patience = arguments['early_stop_patience'])
 
-    inp = Input(shape=(arguments['max_len'],))
-    x = embedding_layer(inp)
-    x1 = SpatialDropout1D(arguments['drop_out_rate'])(x)
-    x = Bidirectional(CuDNNGRU(arguments['units'], return_sequences = True))(x1)
-    x = Conv1D(64, kernel_size = 2, padding = "valid", kernel_initializer = "he_uniform")(x)
-    y = Bidirectional(CuDNNLSTM(arguments['units'], return_sequences = True))(x1)
-    y = Conv1D(64, kernel_size = 2, padding = "valid", kernel_initializer = "he_uniform")(y)
-    avg_pool1 = GlobalAveragePooling1D()(x)
-    max_pool1 = GlobalMaxPooling1D()(x)
-    avg_pool2 = GlobalAveragePooling1D()(y)
-    max_pool2 = GlobalMaxPooling1D()(y)
-    x = concatenate([avg_pool1, max_pool1, avg_pool2, max_pool2])
-    if y_train.ndim == 2:
-        output = Dense(y_train.shape[1], activation='sigmoid')(x)
-    else:
-        output = Dense(1, activation='sigmoid')(x)
 
-    model = Model(inputs = inp, outputs = output)
-    model.compile(loss = "binary_crossentropy", optimizer = Adam(lr = arguments['learning_rate'], decay = arguments['learning_rate_decay']),\
-                  metrics = ["accuracy"])
-    history = model.fit(X_train, y_train, batch_size = arguments['batch_size'], epochs = arguments['epochs'],\
-                        validation_data = (X_valid, y_valid), verbose = 1, callbacks = [ra_val, check_point, early_stop])
-    model = load_model(file_path)
-    return model
+
+    def build_model(self):
+
+        print('Building model...')
+
+        file_path = self.arguments['checkpoint_path']
+        check_point = ModelCheckpoint(file_path, monitor = "val_loss", verbose = 1,
+                                      save_best_only = True, mode = "min")
+        ra_val = RocAucEvaluation(validation_data=(self.X_valid, self.y_valid), interval = 1)
+        early_stop = EarlyStopping(monitor = "val_loss", mode = "min", patience = self.arguments['early_stop_patience'])
+
+        inp = Input(shape=(self.arguments['max_len'],))
+        x = self.embedding_layer(inp)
+        x1 = SpatialDropout1D(self.arguments['drop_out_rate'])(x)
+        x = Bidirectional(CuDNNGRU(self.arguments['units'], return_sequences = True))(x1)
+        x = Conv1D(64, kernel_size = 2, padding = "valid", kernel_initializer = "he_uniform")(x)
+        y = Bidirectional(CuDNNLSTM(self.arguments['units'], return_sequences = True))(x1)
+        y = Conv1D(64, kernel_size = 2, padding = "valid", kernel_initializer = "he_uniform")(y)
+        avg_pool1 = GlobalAveragePooling1D()(x)
+        max_pool1 = GlobalMaxPooling1D()(x)
+        avg_pool2 = GlobalAveragePooling1D()(y)
+        max_pool2 = GlobalMaxPooling1D()(y)
+        x = concatenate([avg_pool1, max_pool1, avg_pool2, max_pool2])
+        if self.y_train.ndim == 2:
+            output = Dense(self.y_train.shape[1], activation='sigmoid')(x)
+        else:
+            output = Dense(1, activation='sigmoid')(x)
+
+        self.model = Model(inputs = inp, outputs = output)
+        self.model.compile(loss = "binary_crossentropy", optimizer = Adam(lr = self.arguments['learning_rate'], decay = self.arguments['learning_rate_decay']),\
+                      metrics = ["accuracy"])
+        history = self.model.fit(self.X_train, self.y_train, batch_size = self.arguments['batch_size'], epochs = self.arguments['epochs'],\
+                            validation_data = (self.X_valid, self.y_valid), verbose = 1, callbacks = [ra_val, check_point, early_stop])
+        self.model = load_model(file_path)
+
+        print('Finished Building BiLstm Model as class attribute class.model.')
+        return self
--- Models/LSTM.py
+++ Models/LSTM.py
@@ -1,19 +1,3 @@
-import logging
-from sklearn.metrics import roc_auc_score, f1_score
-from keras.callbacks import Callback
-
-class RocAucEvaluation(Callback):
-    def __init__(self, validation_data=(), interval=1):
-        super(Callback, self).__init__()
-
-        self.interval = interval
-        self.X_val, self.y_val = validation_data
-
-    def on_epoch_end(self, epoch, logs={}):
-        if epoch % self.interval == 0:
-            y_pred = self.model.predict(self.X_val, verbose=0)
-            score = roc_auc_score(self.y_val, y_pred)
-            print("\n ROC-AUC - epoch: {:d} - score: {:.6f}".format(epoch+1, score))
 
 from keras.optimizers import Adam, RMSprop
 from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
@@ -25,45 +9,56 @@ from keras import initializers, regularizers, constraints, optimizers, layers, c
 from keras import backend as K
 from keras.engine import InputSpec, Layer
 
-arguments = {
-    'max_len': 200,
-    'batch_size': 128,
-    'epochs': 50,
-    'learning_rate': 1e-3,
-    'learning_rate_decay': 0,
-    'units': 128,
-    'drop_out_rate': 0.2,
-    'checkpoint_path': 'best_model.hdf5',
-    'early_stop_patience': 3,
-}
+class Lstm:
 
-def build_lstm_model(X_train, y_train, X_valid, y_valid, arguments):
+    def __init__(self, X_train, y_train, X_valid, y_valid, embedding_layer):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_valid = X_valid
+        self.y_valid = y_valid 
+        self.embedding_layer = embedding_layer
+        self.arguments  = {
+                    'max_len': 200,
+                    'batch_size': 128,
+                    'epochs': 50,
+                    'learning_rate': 1e-3,
+                    'learning_rate_decay': 0,
+                    'units': 128,
+                    'drop_out_rate': 0.2,
+                    'checkpoint_path': 'best_model.hdf5',
+                    'early_stop_patience': 3,
+                }
 
-    print('Building model...')
 
-    file_path = arguments['checkpoint_path']
-    check_point = ModelCheckpoint(file_path, monitor = "val_loss", verbose = 1,
-                                  save_best_only = True, mode = "min")
-    ra_val = RocAucEvaluation(validation_data=(X_valid, y_valid), interval = 1)
-    early_stop = EarlyStopping(monitor = "val_loss", mode = "min", patience = arguments['early_stop_patience'])
-    
-    inp = Input(shape=(arguments['max_len'],))
-    x = embedding_layer(inp)
-    x = CuDNNLSTM(arguments['units'], return_sequences=True, name='lstm_layer')(x)
-    x = GlobalMaxPool1D()(x)
-    x = Dropout(arguments['drop_out_rate'])(x)
-    x = Dense(50, activation="relu")(x)
-    x = Dropout(arguments['drop_out_rate'])(x)
-    # using sigmoid since we are doing six binary classifications
-    if y_train.ndim == 2:
-        output = Dense(y_train.shape[1], activation='sigmoid')(x)
-    else:
-        output = Dense(1, activation='sigmoid')(x)
+    def build_model(self):
 
-    model = Model(inputs = inp, outputs = output)
-    model.compile(loss = "binary_crossentropy", optimizer = Adam(lr = arguments['learning_rate'], decay = arguments['learning_rate_decay']),\
-                  metrics = ["accuracy"])
-    history = model.fit(X_train, y_train, batch_size = arguments['batch_size'], epochs = arguments['epochs'],\
-                        validation_data = (X_valid, y_valid), verbose = 1, callbacks = [ra_val, check_point, early_stop])
-    model = load_model(file_path)
-    return model
+        print('Building model...')
+
+        file_path = self.arguments['checkpoint_path']
+        check_point = ModelCheckpoint(file_path, monitor = "val_loss", verbose = 1,
+                                      save_best_only = True, mode = "min")
+        ra_val = RocAucEvaluation(validation_data=(self.X_valid, self.y_valid), interval = 1)
+        early_stop = EarlyStopping(monitor = "val_loss", mode = "min", patience = self.arguments['early_stop_patience'])
+        
+        inp = Input(shape=(self.arguments['max_len'],))
+        x = self.embedding_layer(inp)
+        x = CuDNNLSTM(self.arguments['units'], return_sequences=True, name='lstm_layer')(x)
+        x = GlobalMaxPool1D()(x)
+        x = Dropout(self.arguments['drop_out_rate'])(x)
+        x = Dense(50, activation="relu")(x)
+        x = Dropout(self.arguments['drop_out_rate'])(x)
+        # using sigmoid since we are doing six binary classifications
+        if self.y_train.ndim == 2:
+            output = Dense(self.y_train.shape[1], activation='sigmoid')(x)
+        else:
+            output = Dense(1, activation='sigmoid')(x)
+
+        self.model = Model(inputs = inp, outputs = output)
+        self.model.compile(loss = "binary_crossentropy", optimizer = Adam(lr = self.arguments['learning_rate'], decay = self.arguments['learning_rate_decay']),\
+                      metrics = ["accuracy"])
+        history = self.model.fit(self.X_train, self.y_train, batch_size = self.arguments['batch_size'], epochs = self.arguments['epochs'],\
+                            validation_data = (self.X_valid, self.y_valid), verbose = 1, callbacks = [ra_val, check_point, early_stop])
+        self.model = load_model(file_path)
+
+        print('Finished Building Lstm Model as class attribute class.model')
+        return self
