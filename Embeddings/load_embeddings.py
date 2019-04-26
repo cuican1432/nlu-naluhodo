15c8e0bfee85b24675e1390f387eb172473db551
Elaine <cc5048@nyu.edu>
2019-04-26 13:18:52 -0400

    edited

--- Embeddings/load_embeddings.py
+++ Embeddings/load_embeddings.py
@@ -44,72 +44,72 @@ class Dataset:
 	    
 		def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
 
-	    word2vec = dict(get_coefs(*o.strip().split(" ")) for o in tqdm(open(self.embedding_path)))
-	    
-	    print('Found %s word vectors.' % len(word2vec))
+		word2vec = dict(get_coefs(*o.strip().split(" ")) for o in tqdm(open(self.embedding_path)))
 
-	    # prepare text samples and their labels
-	    print('Loading email contents...')
-	    # extract the comments, fill NaN with some values
-	    contents = self.dataset[self.content_column].fillna("DUMMY_VALUE").values
-	    labels = self.dataset[self.label_column].values
-	    
-	    if self.data_index == None:
-	        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(contents, labels, test_size = 0.2, random_state=0)
-	        self.X_train, self.X_valid, self.y_train, self.y_valid = train_test_split(self.X_train, self.y_train, test_size = 0.125, random_state=0)
-	    else:
-	        self.X_train = contents[self.data_index[0]]
-	        self.y_train = labels[self.data_index[0]]
-	        self.X_valid = contents[self.data_index[1]]
-	        self.y_valid = labels[self.data_index[1]]
-	        self.X_test = contents[self.data_index[2]]
-	        self.y_test = labels[self.data_index[2]]
+		print('Found %s word vectors.' % len(word2vec))
 
-	    # convert the sentences (strings) into integers, thus they can be used as index later on
-	    tokenizer = Tokenizer(num_words=self.max_features)
-	    tokenizer.fit_on_texts(self.X_train)
-	    #sequences = tokenizer.texts_to_sequences(sentences)
-	    self.X_train_seq = tokenizer.texts_to_sequences(self.X_train)
-	    self.X_valid_seq = tokenizer.texts_to_sequences(self.X_valid)
-	    self.X_test_seq = tokenizer.texts_to_sequences(self.X_test)
+		# prepare text samples and their labels
+		print('Loading email contents...')
+		# extract the comments, fill NaN with some values
+		contents = self.dataset[self.content_column].fillna("DUMMY_VALUE").values
+		labels = self.dataset[self.label_column].values
 
-	    print("max sequence length:", max(len(s) for s in self.X_train_seq))
-	    print("min sequence length:", min(len(s) for s in self.X_train_seq))
-	    s = sorted(len(s) for s in self.X_train_seq)
-	    print("median sequence length:", s[len(s) // 2])
+		if self.data_index == None:
+		    self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(contents, labels, test_size = 0.2, random_state=0)
+		    self.X_train, self.X_valid, self.y_train, self.y_valid = train_test_split(self.X_train, self.y_train, test_size = 0.125, random_state=0)
+		else:
+		    self.X_train = contents[self.data_index[0]]
+		    self.y_train = labels[self.data_index[0]]
+		    self.X_valid = contents[self.data_index[1]]
+		    self.y_valid = labels[self.data_index[1]]
+		    self.X_test = contents[self.data_index[2]]
+		    self.y_test = labels[self.data_index[2]]
 
-	    # get word -> integer mapping
-	    word2idx = tokenizer.word_index
-	    print('Found %s unique tokens.' % len(word2idx))
+		# convert the sentences (strings) into integers, thus they can be used as index later on
+		tokenizer = Tokenizer(num_words=self.max_features)
+		tokenizer.fit_on_texts(self.X_train)
+		#sequences = tokenizer.texts_to_sequences(sentences)
+		self.X_train_seq = tokenizer.texts_to_sequences(self.X_train)
+		self.X_valid_seq = tokenizer.texts_to_sequences(self.X_valid)
+		self.X_test_seq = tokenizer.texts_to_sequences(self.X_test)
 
-	    # pad sequences so that we get a N x T matrix
-	    # Keras take care of the 0 only for padding purpose 
-	  
-	    self.X_train = pad_sequences(self.X_train_seq, maxlen=self.max_len)
-	    self.X_valid = pad_sequences(self.X_valid_seq, maxlen=self.max_len)
-	    self.X_test = pad_sequences(self.X_test_seq, maxlen=self.max_len)
-	    print('Shape of data tensor:', self.X_train.shape)
+		print("max sequence length:", max(len(s) for s in self.X_train_seq))
+		print("min sequence length:", min(len(s) for s in self.X_train_seq))
+		s = sorted(len(s) for s in self.X_train_seq)
+		print("median sequence length:", s[len(s) // 2])
 
-	    # prepare embedding matrix
-	    print('Filling pre-trained embeddings...')
-	    num_words = min(self.max_features, len(word2idx) + 1)
-	    embedding_matrix = np.zeros((num_words, self.embed_size))
-	    for word, i in word2idx.items():
-	        if i < self.max_features:
-	            embedding_vector = word2vec.get(word)
-	        if embedding_vector is not None:
-	          # words not found in embedding index will be all zeros.
-	            embedding_matrix[i] = embedding_vector
+		# get word -> integer mapping
+		word2idx = tokenizer.word_index
+		print('Found %s unique tokens.' % len(word2idx))
 
-	    # load pre-trained word embeddings into an Embedding layer
-	    # note that we set trainable = False so as to keep the embeddings fixed
-	    self.embedding_layer = Embedding(
-	      min(self.max_features, embedding_matrix.shape[0]),
-	      self.embed_size,
-	      weights=[embedding_matrix],
-	      input_length=self.max_len,
-	        # don't want to make the embeddding updated during the procedure
-	      trainable=False
-	    )
+		# pad sequences so that we get a N x T matrix
+		# Keras take care of the 0 only for padding purpose 
 
-	    return self
+		self.X_train = pad_sequences(self.X_train_seq, maxlen=self.max_len)
+		self.X_valid = pad_sequences(self.X_valid_seq, maxlen=self.max_len)
+		self.X_test = pad_sequences(self.X_test_seq, maxlen=self.max_len)
+		print('Shape of data tensor:', self.X_train.shape)
+
+		# prepare embedding matrix
+		print('Filling pre-trained embeddings...')
+		num_words = min(self.max_features, len(word2idx) + 1)
+		embedding_matrix = np.zeros((num_words, self.embed_size))
+		for word, i in word2idx.items():
+		    if i < self.max_features:
+		        embedding_vector = word2vec.get(word)
+		    if embedding_vector is not None:
+		      # words not found in embedding index will be all zeros.
+		        embedding_matrix[i] = embedding_vector
+
+		# load pre-trained word embeddings into an Embedding layer
+		# note that we set trainable = False so as to keep the embeddings fixed
+		self.embedding_layer = Embedding(
+		  min(self.max_features, embedding_matrix.shape[0]),
+		  self.embed_size,
+		  weights=[embedding_matrix],
+		  input_length=self.max_len,
+		    # don't want to make the embeddding updated during the procedure
+		  trainable=False
+		)
+
+		return self
